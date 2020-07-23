d37c81fe1de4fe3417de1dbd47f75bb85bccb9ed
Emanuele Pace <emanuele.10@outlook.it>
2020-07-23 15:20:08 +0100

    Encapsulation

--- src/index.js
+++ src/index.js
@@ -14,11 +14,8 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-
 // define grid size
 const GRID_SIZE = 30;
-
-
 
 // define game area size
 const GAME_HEIGHT = 9 * GRID_SIZE;
@@ -43,19 +40,12 @@ let ctx = canvas.getContext("2d");
 let player = new Player(GAME_HEIGHT, GAME_WIDTH, GRID_SIZE);
 // input instance
 let input = new Input(player);
-let map = new Map(player, ctx);
+let map = new Map(player, ctx, gridMap, GRID_SIZE);
 
 // modify canvas size
 canvas.height = GAME_HEIGHT;
 canvas.width = GAME_WIDTH;
 
-
-let drawMap = function () {
-  for (let index = 0; index < gridMap.length; index++) {
-    ctx.fillStyle = (gridMap[index] === 1) ? "#B7DFE1" : "#CCF2F4";
-    ctx.fillRect((index % 16) * GRID_SIZE, Math.floor(index / 16) * GRID_SIZE, GRID_SIZE, GRID_SIZE);
-  }
-};
 
 // game loop
 let lastTime = 0;
@@ -63,7 +53,7 @@ function gameLoop(timestamp) {
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-  drawMap()
+  map.drawMap()
   player.update(deltaTime);
   map.isWithinX()
   player.draw(ctx);
