94e8abe22e97aa70d1bc60842fc52a4c4a9ad0c5
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-23 15:31:20 +0100

    tests for player move right, left and player start

--- src/__test__/player.test.js
+++ src/__test__/player.test.js
@@ -1,16 +1,47 @@
 import Player from "../js/player";
+import { wait } from "@testing-library/react";
 
 describe("Player", () => {
   let player;
+  let moveRight;
+  let moveLeft;
+
 
   beforeEach(() => {
-    player = new Player(600, 800)
+    player = new Player(600, 800, 30)
+    moveRight = ["player.moveRight()"]
+    moveLeft = ["player.moveLeft()"]
   })
+
+  wait((ms) => {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    ); 
+  });
 
   test("test player construncts width and height", () => {
     expect(player.gameWidth).toEqual(800)
     expect(player.gameHeight).toEqual(600)
   } )
+
+  test("player can move to the right", () => {
+    player.moveRight()
+    expect(player.vel.x).toEqual(10)
+  } )
+
+  test("player can move to the left", () => {
+    player.moveLeft()
+    expect(player.vel.x).toEqual(-10)
+  } )
+
+  test("player starts moving when passed moves", () => {
+    player.start(moveRight)
+    expect(player.vel.x).toEqual(10)
+    player.start(moveLeft)
+    expect(player.vel.x).toEqual(-10)
+  } )
+
+
 
 }) // describe
 
