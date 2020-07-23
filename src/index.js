efbfa4ab5eb443990677197417fe3ab9d36e189f
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-23 11:45:02 +0100

    created grid

--- src/index.js
+++ src/index.js
@@ -13,18 +13,47 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-let userInput = document.getElementById("userInput");
 let canvas = document.getElementById("gameArea");
 let ctx = canvas.getContext("2d");
 
+const GRID_SIZE = 30;
 
 // define game area size
-const GAME_HEIGHT = 600;
-const GAME_WIDTH = 800;
+const GAME_HEIGHT = 9 * GRID_SIZE;
+const GAME_WIDTH = 16 * GRID_SIZE;
 
 // modify canvas size
 canvas.height = GAME_HEIGHT;
 canvas.width = GAME_WIDTH;
+
+let gridMap = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+               1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+               1,0,1,1,1,0,0,1,0,0,1,0,0,1,0,1,
+               1,0,0,1,0,1,0,1,0,1,0,1,0,1,0,1,
+               1,0,0,1,0,0,0,1,0,1,1,1,0,1,0,1,
+               1,0,0,1,0,1,0,1,0,1,0,0,0,0,0,1,
+               1,0,0,1,0,1,0,1,0,0,1,1,0,1,0,1,
+               1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+               1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+
+let drawMap = function() {
+for (let index = 0; index < gridMap.length; index ++) {
+ctx.fillStyle = (gridMap[index] === 1) ? "#B7DFE1" : "#CCF2F4";
+ctx.fillRect((index % 16) * GRID_SIZE, Math.floor(index/16) * GRID_SIZE, GRID_SIZE, GRID_SIZE);
+}
+};
+
+
+
+// define game area size
+// const GAME_HEIGHT = 600;
+// const GAME_WIDTH = 800;
+
+
+
+// // modify canvas size
+// canvas.height = GAME_HEIGHT;
+// canvas.width = GAME_WIDTH;
 
 let player = new Player(GAME_HEIGHT, GAME_WIDTH);
 let input = new Input(player);
@@ -35,9 +64,10 @@ function gameLoop(timestamp) {
   let deltaTime = timestamp - lastTime;
   lastTime = timestamp;
   ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
+  drawMap()
   player.update(deltaTime);
   player.draw(ctx);
+
 
   requestAnimationFrame(gameLoop);
 }
--- src/js/player.js
+++ src/js/player.js
@@ -30,6 +30,7 @@ export default class Player {
   }
 
   draw(ctx) {
+    ctx.fillStyle = 'black'
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
 
