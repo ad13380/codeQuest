c78921b17cbf4e6f6757051426dfe13daf192bbc
Emanuele Pace <emanuele.10@outlook.it>
2020-07-24 14:51:13 +0100

    new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,13 @@
 import React from "react";
 import "../App.css";
-import Canvas from "./Canvas"
+// import Canvas from "./Canvas"
 
 function App() {
   return (
-    <Canvas/>
-    // <div className="App">
-    //   <canvas id="gameArea"></canvas>
-    //   <br />
-    //   <textarea id="userInput" name="name" rows="20" cols="40"></textarea>
-    //   <br />
-    //   <button id="play" type="submit">
-    //     Play!
-    //   </button>
-    // </div>
+    <div>
+      <h1>CodeQuest()</h1>
+      <p> Just have fun </p>
+    </div>
   );
 }
 
---
+++ src/components/Play.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Canvas from "./Canvas"
+
+function Play() {
+  return <Canvas/>
+}
+
+export default Play;
