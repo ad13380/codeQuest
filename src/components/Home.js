34ac6dc3f23bd95a7a3c1960916147d528d04dcb
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-27 15:33:30 +0100

    basic format for home component set

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className='center mw5 mw7-ns'>
+      <div className='tc ba b--dashed bw3'>
+        <p>Welcome to CodeQuest()! You are tasked with helping our hero </p>
+      </div>
+    </div>
+  )
+}
+
+export default Home;
