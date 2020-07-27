64d770ab418a2701af14380dd52e03a9934ab09e
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-27 16:04:53 +0100

    added padding to both divs

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 function Home() {
   return (
-    <div className='center mw5 mw7-ns'>
-      <div className='tc ba b--dashed bw3'>
-        <p>Welcome to CodeQuest()! You are tasked with helping our hero </p>
+    <div className='center mw5 mw7-ns pa3'>
+      <div className='tc ba b--dashed bw3 pa3'>
+        <p>Welcome to CodeQuest(), a platformer with a difference! <br></br><br></br>
+          Unlike other heros, ours does not understand normal commands. To get them to move we need to use code!<br></br><br></br>
+          Your task is to help our hero make it across countless levels using very specific commands, do you thing you are up for the challenge?<br></br><br></br>
+          Before you start make sure to check out the instructions page so you know what you are doing! or you know, dive right in and play, who am I to judge!
+        </p>
       </div>
     </div>
   )
