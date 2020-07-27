477f7dd5f9f8f37b09dd57f7dcc704e39e7ca12f
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-27 16:39:21 +0100

    fixed failing test for instructions

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Instructions() {
   return (
-    <div className="tc">
-    <p> To play CodeQuest() .... </p>
+    <div className='center mw5 mw7-ns pa3'>
+      <div className='tc ba b--dashed bw3 pa3'>
+        <p>Instructions<br /><br />
+        As you read before our hero is a touch stubborn, they refuse to move unless you tell them to using code. <br />
+        </p>
+      </div>
     </div>
   )
 }
