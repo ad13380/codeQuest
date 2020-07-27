3addc058d8ae7760d3bcce61876d4ae7946c2542
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-27 16:08:32 +0100

    added basic instruction to play page

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -3,7 +3,14 @@ import React from "react";
 function TextArea() {
   return (
     <div>
-      <span id='ErrorMessage'></span>
+      <div>
+        <br />
+        Make sure you are using the correct commands to move our hero!<br /><br />
+        player.moveRight()<br />
+        player.moveLeft()<br />
+        player.jumpRight()<br />
+        player.jumpLeft()<br />
+      </div>
       <br />
       <textarea id="userInput" name="name" rows="15" cols="30"></textarea>
       <br />
@@ -12,7 +19,9 @@ function TextArea() {
       </button>
       <button id="reset" type="submit">
         Back to Start
-      </button>
+      </button><br />
+      <span id='ErrorMessage'></span>
+
     </div>
   );
 }
