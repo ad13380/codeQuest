507dc7a83e216fc7a0a9e56ba9105777773308ac
Emanuele Pace <emanuele.10@outlook.it>
2020-07-27 15:43:34 +0100

    fill about us page

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,8 +3,44 @@ import React from "react";
 function AboutUs() {
   return (
     <div className="tc">
-    <p> About us: Bene, Ema, Chris, Anthony </p>
+    <h3> About us: </h3>
+
+      <div>
+        <img />
+        <p> Benedetta Arinci
+        </p>
+        <a href ={'https://github.com/benearinci'}>Bene's GitHub</a>
+
+      </div>
+
+      <div>
+        <img />
+        <p> Anthony Donovan
+        </p>
+        <a href ={'https://github.com/ad13380'}>Anthony's GitHub</a>
+
+
+      </div>
+
+      <div>
+        <img />
+        <p> Chris Cooney
+        </p>
+        <a href ={'https://github.com/ChrisCooney05'}>Chris' GitHub</a>
+
+      </div>
+
+      <div>
+        <img />
+        <p> Emanuele Pace
+        </p>
+        <a href ={'https://github.com/Emanuele-20'}>Emanuele's GitHub</a>
+
+      </div>
+
     </div>
+
+
   )
 }
 
