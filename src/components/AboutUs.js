8d831ee11dddc42f0cc4e983a45f12af20d1f0ac
Bene Arinci <benedetta.arinci@gmail.com>
2020-07-27 15:59:12 +0100

    added profile pictures in aboutUs

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -6,7 +6,7 @@ function AboutUs() {
     <h3> About us: </h3>
 
       <div>
-        <img />
+        <img alt='bene' src="./assets/Bene.jpeg" width="200" height="200"/>
         <p> Benedetta Arinci
         </p>
         <a href ={'https://github.com/benearinci'}>Bene's GitHub</a>
@@ -14,7 +14,7 @@ function AboutUs() {
       </div>
 
       <div>
-        <img />
+      <img alt='anthony' src="./assets/Anthony.jpeg" width="200" height="200"/>
         <p> Anthony Donovan
         </p>
         <a href ={'https://github.com/ad13380'}>Anthony's GitHub</a>
@@ -23,7 +23,7 @@ function AboutUs() {
       </div>
 
       <div>
-        <img />
+        <img alt='chris' src="./assets/Chris.jpeg" width="200" height="200"/>
         <p> Chris Cooney
         </p>
         <a href ={'https://github.com/ChrisCooney05'}>Chris' GitHub</a>
@@ -31,7 +31,7 @@ function AboutUs() {
       </div>
 
       <div>
-        <img />
+      <img alt='ema' src="./assets/Ema.png" width="200" height="200"/>
         <p> Emanuele Pace
         </p>
         <a href ={'https://github.com/Emanuele-20'}>Emanuele's GitHub</a>
