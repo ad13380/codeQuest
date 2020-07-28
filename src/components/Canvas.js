96a6c72e19fa00e686982a1b3648085c4191bfca
Emanuele Pace <emanuele.10@outlook.it>
2020-07-28 14:38:17 +0100

    Moving text area

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -3,10 +3,10 @@ import TextArea from "./TextArea"
 
 function Canvas() {
   return (
-    <div className='pa31 flex justify-around' >
+    <div className='pa31 flex justify-around flex-wrap' >
       {/* <canvas className='fr w-50 ' id="gameArea" data-testid='canvas'></canvas> */}
-      <canvas className='fr' id="gameArea" data-testid='canvas' style={{height: "80%", width: "55%",position: "relative"}}></canvas>
       <TextArea />
+      <canvas className='fr' id="gameArea" data-testid='canvas' style={{height: "80%", width: "55%",position: "relative"}}></canvas>
     </div>
   )
 }
